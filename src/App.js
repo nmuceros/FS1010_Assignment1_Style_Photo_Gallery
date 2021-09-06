5dfbf103884083c59dd7f4a8a8cd7d3892e114de
nmuceros <nolita.muceros@gmail.com>
2021-09-06 18:11:44 -0400

    adds image texts

--- src/App.js
+++ src/App.js
@@ -12,7 +12,18 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const images = [logo1, logo2, logo3, logo4, logo5, logo6];
+  // const images = [logo1, logo2, logo3, logo4, logo5, logo6];
+
+  const images = [
+    {pic: logo1, site: "Toronto", desc: "The Skyline"},
+    {pic: logo2, site: "Old City Hall", desc: "The Clock Tower"},
+    {pic: logo3, site: "The CN Tower", desc: "One of the Worlds Tallest Towers"},
+    {pic: logo4, site: "Royal Ontario Museum", desc: "Most Visited Museum in Canada"},
+    {pic: logo5, site: "The Rogeres Center", desc: "Toronto's Iconic Skydome"},
+    {pic: logo6, site: "Toronto City Hall", desc: "A Brutalist Architecture Masterpiece"},  
+  ];
+
+
   const [selectedImage, setSelectedImage] = useState(null);
 
   return (
@@ -25,7 +36,18 @@ function App() {
       </header>
       <section class="grid">
         {
-          images.map(image => <img src={image} alt="logo" onClick={()=>setSelectedImage(image)} />)
+          images.map(image => 
+
+            <div className="galleryItem-container">
+
+                <div className="img-container">
+                     <img src={image.pic} alt="logo" onClick={()=>setSelectedImage(image)}/>
+                </div>
+                <div className="siteTxt-container">{image.site}</div>
+                <div className="siteDescTxt-container">{image.desc}</div>
+            </div>
+
+          )
         }
       </section>
       <div id='overlay' style={{visibility: selectedImage ? 'visible': 'hidden'}}>
