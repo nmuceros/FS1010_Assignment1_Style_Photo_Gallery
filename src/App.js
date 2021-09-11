222e0651fb4cc830180cc036f1fda4890d8b1db7
nmuceros <nolita.muceros@gmail.com>
2021-09-11 19:06:40 -0400

    adds social

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 //import logo from './logo.svg';
 //import logo from '../src/images/old-city-hall.png';
-import logo1 from '../src/images/city-skyline.jpg';
-import logo2 from '../src/images/old-city-hall.jpg';
-import logo3 from '../src/images/cn-tower.jpg';
-import logo4 from '../src/images/rom.jpg';
-import logo5 from '../src/images/rogers-center.jpg';
-import logo6 from '../src/images/new-city-hall.jpg';
-
+import image1 from '../src/images/city-skyline.jpg';
+import image2 from '../src/images/old-city-hall.jpg';
+import image3 from '../src/images/cn-tower.jpg';
+import image4 from '../src/images/rom.jpg';
+import image5 from '../src/images/rogers-center.jpg';
+import image6 from '../src/images/new-city-hall.jpg';
+import twitter from '../src/images/twitter.svg';
+import facebook from '../src/images/facebook.svg';
+import linkedin from '../src/images/linkedin.svg';
+import instagram from '../src/images/instagram.svg';
 
 import './App.css';
 import { useState } from 'react';
@@ -15,16 +18,26 @@ function App() {
   // const images = [logo1, logo2, logo3, logo4, logo5, logo6];
 
   const images = [
-    {pic: logo1, site: "Toronto", desc: "The Skyline"},
-    {pic: logo2, site: "Old City Hall", desc: "The Clock Tower"},
-    {pic: logo3, site: "The CN Tower", desc: "One of the Worlds Tallest Towers"},
-    {pic: logo4, site: "Royal Ontario Museum", desc: "Most Visited Museum in Canada"},
-    {pic: logo5, site: "The Rogeres Center", desc: "Toronto's Iconic Skydome"},
-    {pic: logo6, site: "Toronto City Hall", desc: "A Brutalist Architecture Masterpiece"},  
+    {pic: image1, site: "Toronto", desc: "The Skyline"},
+    {pic: image2, site: "Old City Hall", desc: "The Clock Tower"},
+    {pic: image3, site: "The CN Tower", desc: "One of the Worlds Tallest Towers"},
+    {pic: image4, site: "Royal Ontario Museum", desc: "Most Visited Museum in Canada"},
+    {pic: image5, site: "The Rogeres Center", desc: "Toronto's Iconic Skydome"},
+    {pic: image6, site: "Toronto City Hall", desc: "A Brutalist Architecture Masterpiece"},  
   ];
+
+  const social = [
+    {icon: twitter, link: "https://twitter.com/?lang=en"},
+    {icon: facebook, link: "https://www.facebook.com/"},
+    {icon: linkedin, link: "https://ca.linkedin.com/"},
+    {icon: instagram, link: "https://www.instagram.com/"}];
+
+
+  
 
 
   const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedSocial, setSelectedSocial] = useState(null);
 
   return (
     <div className="App">
@@ -40,15 +53,30 @@ function App() {
 
             <div className="galleryItem-container" onClick={() => setSelectedImage(image.pic)}>
 
-                  <img className="img" src={image.pic} alt="logo"/>
+                  <img className="img" src={image.pic} alt="image" />
                   <div className="imgTxt-container"> 
-                      <div className="siteTxt-container">{image.site}</div>
+                      <div className="siteTxt-container">{image.site}</div>                      
                       <div className="siteDescTxt-container">{image.desc}</div>
                   </div>
             </div>
           )
         }
       </section>
+
+
+      <footer className="footer"> 
+          <div className="social-container">
+            {
+              social.map(social => 
+                  <div className="social-image-container">
+                    <a href={social.link}>
+                      <img className="img-social" src={social.icon} alt="social-image" onClick={() => setSelectedSocial(social.icon)} />
+                    </a>
+                  </div>
+              )
+            }
+          </div>
+      </footer>
       <div id='overlay' 
           style={{
             visibility: selectedImage ? 'visible': 'hidden',
